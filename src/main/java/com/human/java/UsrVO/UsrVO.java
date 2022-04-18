84345dd8d2927c52982a2542f6461186fd162ea8
deucal <80443356+deucal@users.noreply.github.com>
2022-04-18 20:54:05 +0900

    4/18 8시 50분 매퍼 오류 해결

--- src/main/java/com/human/java/UsrVO/UsrVO.java
+++ src/main/java/com/human/java/UsrVO/UsrVO.java
@@ -15,4 +15,90 @@ public class UsrVO {
 	String USR_POINT;
 	String USR_ADMIN;
 	String USR_OUT;
+	
+	
+	public String getUSR_ID() {
+		return USR_ID;
+	}
+	public void setUSR_ID(String uSR_ID) {
+		USR_ID = uSR_ID;
+	}
+	public String getUSR_PW() {
+		return USR_PW;
+	}
+	public void setUSR_PW(String uSR_PW) {
+		USR_PW = uSR_PW;
+	}
+	public String getUSR_NAME() {
+		return USR_NAME;
+	}
+	public void setUSR_NAME(String uSR_NAME) {
+		USR_NAME = uSR_NAME;
+	}
+	public String getUSR_NICK() {
+		return USR_NICK;
+	}
+	public void setUSR_NICK(String uSR_NICK) {
+		USR_NICK = uSR_NICK;
+	}
+	public String getUSR_TEL() {
+		return USR_TEL;
+	}
+	public void setUSR_TEL(String uSR_TEL) {
+		USR_TEL = uSR_TEL;
+	}
+	public String getUSR_EMAIL() {
+		return USR_EMAIL;
+	}
+	public void setUSR_EMAIL(String uSR_EMAIL) {
+		USR_EMAIL = uSR_EMAIL;
+	}
+	public String getUSR_ADDR1() {
+		return USR_ADDR1;
+	}
+	public void setUSR_ADDR1(String uSR_ADDR1) {
+		USR_ADDR1 = uSR_ADDR1;
+	}
+	public String getUSR_ADDR2() {
+		return USR_ADDR2;
+	}
+	public void setUSR_ADDR2(String uSR_ADDR2) {
+		USR_ADDR2 = uSR_ADDR2;
+	}
+	public String getUSR_ADDR3() {
+		return USR_ADDR3;
+	}
+	public void setUSR_ADDR3(String uSR_ADDR3) {
+		USR_ADDR3 = uSR_ADDR3;
+	}
+	public String getUSR_VEGE() {
+		return USR_VEGE;
+	}
+	public void setUSR_VEGE(String uSR_VEGE) {
+		USR_VEGE = uSR_VEGE;
+	}
+	public String getUSR_GRADE() {
+		return USR_GRADE;
+	}
+	public void setUSR_GRADE(String uSR_GRADE) {
+		USR_GRADE = uSR_GRADE;
+	}
+	public String getUSR_POINT() {
+		return USR_POINT;
+	}
+	public void setUSR_POINT(String uSR_POINT) {
+		USR_POINT = uSR_POINT;
+	}
+	public String getUSR_ADMIN() {
+		return USR_ADMIN;
+	}
+	public void setUSR_ADMIN(String uSR_ADMIN) {
+		USR_ADMIN = uSR_ADMIN;
+	}
+	public String getUSR_OUT() {
+		return USR_OUT;
+	}
+	public void setUSR_OUT(String uSR_OUT) {
+		USR_OUT = uSR_OUT;
+	}
 }
