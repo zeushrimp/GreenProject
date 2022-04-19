d8f9c80f0b7f39c401deaccc8c90864e0499eae5
deucal <80443356+deucal@users.noreply.github.com>
2022-04-19 13:05:39 +0900

    4/19 수완 오류해결

--- src/main/java/com/human/java/RcpVO/RcpVO.java
+++ src/main/java/com/human/java/RcpVO/RcpVO.java
@@ -4,16 +4,16 @@ import java.util.Date;
 
 public class RcpVO {
 	
-	int RCP_PK;
-	String RCP_TITLE;
-	String RCP_CONTENT;
-	Date RCP_REG;
-	int RCP_HIT;
-	int RCP_like;
-	int RCP_COM;
-	String RCP_VEGE;
-	String RCP_CT;
-	String USR_ID;
+//	int RCP_PK;
+//	String RCP_TITLE;
+//	String RCP_CONTENT;
+//	Date RCP_REG;
+//	int RCP_HIT;
+//	int RCP_like;
+//	int RCP_COM;
+//	String RCP_VEGE;
+//	String RCP_CT;
+//	String USR_ID;
 	
 	
 	
