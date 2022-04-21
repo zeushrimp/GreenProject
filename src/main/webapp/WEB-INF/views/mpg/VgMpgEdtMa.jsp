0b62dd8f3653f688b4a1ac65dfbb2f638efd6d47
zeushrimp <chung_103@naver.com>
2022-04-21 19:48:15 +0900

    homecon젝

--- src/main/webapp/WEB-INF/views/mpg/VgMpgEdtMa.jsp
+++ src/main/webapp/WEB-INF/views/mpg/VgMpgEdtMa.jsp
@@ -1,30 +1,70 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
-<body>
+
+<body >
 	<div class="row">
-		<div class="modal" id="modal5" tabindex="-1"data-toggle="modal5">
+		<div class="modal" id="modal5" tabindex="-1">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
 						<button class="close" data-dismiss="modal">&times;</button>
 					</div>
-					<div class="modal-body" style="text-align: center;">
-						마이페이지의 주소 변경 모달 페이지입니다.<br> <br> 
-						<img
-							src="/resources/images/admin.png" id="imagepreview"
-							style="width: 256px; height: 256px;">
+					<div style="text-align: center;">
+						<div style="display: inline-block;">
+							<img src="/resources/images/addr.png" id="imagepreview"
+								style="width: 256px; height: 256px;">
+						</div>
+						<div class="modal-body"
+							style="text-align: center; display: inline-block;">
+							<div>
+								<h1 style="text-align: center;">${mpgdata.USR_NAME}님
+									이사하셨나봐요!</h1>
+								<h5 style="text-align: center;">
+									주소를 변경을 위해 <span style="color: #ff7b74;">우편번호찾기</span> 버튼을
+									눌러주세요~<br>
+								</h5>
+								<br>
+								<form id="Mf_addr" action="">
+									<div>
+										<input type="text" name="USR_ADDR1" id="sample6_postcode1"
+											placeholder="우편번호" readonly> <input type="button"
+											class="close" onclick="sample6_execDaumPostcode1()"
+											value="우편번호 찾기"><br>
+									</div>
+									<div>
+										<input type="text" name="USR_ADDR2" id="sample6_address1"
+											placeholder="주소" readonly>
+									</div>
+									<div>
+										<span id="guide1" style="color: #999; display: none"></span> <input
+											type="text" name="USR_ADDR3" id="sample6_detailAddress1"
+											placeholder="상세주소"> <input type="hidden"
+											id="sample6_extraAddress1" placeholder="참고항목">
+									</div>
+									<span id="s_result3" style="font-size: 12px; color: red;">상세
+										주소를 입력해주세요.</span> <input type="button"
+										class="close VgMpg_modify_addrclose" data-dismiss="modal"
+										value="확인" disabled="disabled" onclick="Mf_addrsubmit()">
+								</form>
+
+
+
+							</div>
+						</div>
+
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-<a href="VgMpgMain.jsp">VgMpgMain로 이동</a>
+	<script
+		src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
 
 </body>
 </html>
--- src/main/webapp/resources/js/Mpg.js
+++ src/main/webapp/resources/js/Mpg.js
@@ -1,4 +1,4 @@
-식('.checkT input[type="checkbox"]').click(function() {
+('.checkT input[type="checkbox"]').click(function() {
 	var tmpp = $(this).prop('checked');
 	// this를 사용하여 클릭한 checkbox 가 체크되도록 설정
 	if (tmpp) {
@@ -168,32 +168,114 @@ function fn_compare_cr_pwd2() {
 
 
 //비밀번호변경-submit
-function Mf_pwsubmit(){
-	$('.VgMpg_modify_pwclose').click(function(){
-		$('#Mf_pw').submit();		
+function Mf_pwsubmit() {
+	$('.VgMpg_modify_pwclose').click(function() {
+		$('#Mf_pw').submit();
 	})
 	alert("비밀번호가 변경되었습니다.")
-	
+
 }
 
 //닉네임변경-submit
-function Mf_nksubmit(){
-	$('.VgMpg_modify_nkclose').click(function(){
-		$('#Mf_nk').submit();		
+function Mf_nksubmit() {
+	$('.VgMpg_modify_nkclose').click(function() {
+		$('#Mf_nk').submit();
 	})
 	alert("닉네임이 변경되었습니다.")
-	
+
 }
 
 //채식변경-submit
-function Mf_vgsubmit(){
-	$('.VgMpg_modify_vgclose').click(function(){
-		$('#Mf_vg').submit();		
+function Mf_vgsubmit() {
+	$('.VgMpg_modify_vgclose').click(function() {
+		$('#Mf_vg').submit();
 	})
 	alert("채식종류가 변경되었습니다.")
-	
+
+}
+
+//주소변경-submit
+function Mf_addrsubmit() {
+	$('.VgMpg_modify_addrclose').click(function() {
+
+		$('#Mf_addr').submit();
+	})
+	alert("주소가 변경되었습니다.")
+
 }
 
 
+
+
+function sample6_execDaumPostcode1() {
+
+	new daum.Postcode({
+		oncomplete: function(data) {
+			// 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+			// 각 주소의 노출 규칙에 따라 주소를 조합한다.
+			// 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+			var addr = ''; // 주소 변수
+			var extraAddr = ''; // 참고항목 변수
+
+			//사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+			if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+				addr = data.roadAddress;
+			} else { // 사용자가 지번 주소를 선택했을 경우(J)
+				addr = data.jibunAddress;
+			}
+
+			// 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+			if (data.userSelectedType === 'R') {
+				// 법정동명이 있을 경우 추가한다. (법정리는 제외)
+				// 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+				if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+					extraAddr += data.bname;
+				}
+				// 건물명이 있고, 공동주택일 경우 추가한다.
+				if (data.buildingName !== '' && data.apartment === 'Y') {
+					extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+				}
+				// 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+				if (extraAddr !== '') {
+					extraAddr = ' (' + extraAddr + ')';
+				}
+				// 조합된 참고항목을 해당 필드에 넣는다.
+				document.getElementById("sample6_extraAddress1").value = extraAddr;
+
+			} else {
+				document.getElementById("sample6_extraAddress1").value = '';
+			}
+
+			// 우편번호와 주소 정보를 해당 필드에 넣는다.
+			document.getElementById('sample6_postcode1').value = data.zonecode;
+			document.getElementById("sample6_address1").value = addr;
+			// 커서를 상세주소 필드로 이동한다.
+			document.getElementById("sample6_detailAddress1").focus();
+		}
+	}).open();
+}
+
+
+function addr_not_null_check() {
+	var $woopyeon = $('#sample6_postcode1');
+	var $sangsae = $('#sample6_detailAddress1');
+	var $text = $('s_result3');
+
+	if ($woopyeon != null) {
+
+		$text.text("상세주소를 입력해주세요");
+	} if ($sangsae != null) {
+		$text.text("확인버튼을 눌러주세요").css('color', 'green');
+
+	} else if ($sangsae != null&&$woopyeon == null) {
+		$text.text("우편번호 찾기로 주소를 넣어주세요");
+
+	}
+
+
+
+
+};
 
 
