0d7061cf4b2088eec1623847517c7cab09ca0505
hasta <hasta@DESKTOP-CD9CCIE>
2022-05-11 12:23:10 +0900

    신고 기능 alert 잘 안뜨는거 빼고 대략 마무리

--- src/main/java/com/human/java/CmuDAO/CmuDao.java
+++ src/main/java/com/human/java/CmuDAO/CmuDao.java
@@ -46,10 +46,10 @@ public interface CmuDao {
 	// 커뮤니티 게시글 신고하기
 	public void cmu_report_insert(CmuVO cmuvo);
 
-    // 커뮤니티 게시글 신고 중복 체크
-//	public int cmu_report_check(CmuVO cmuvo);
-
 	// cmu 테이블의 cmu_report 컬럼의 값에 1이라는 값을 삽입 
-	public void cmu_report_columm_one(CmuVO cmuvo);
+	public void cmu_report_columm_add(CmuVO cmuvo);
+	
+    // 커뮤니티 게시글 신고 중복 체크
+	public int cmu_report_check(CmuVO cmuvo);
 	
 }
